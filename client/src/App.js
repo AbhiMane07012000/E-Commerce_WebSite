d96fde38124b670f4133f7420ba83708f4c5feb2
AbhiMane07012000 <abhi.mane07012000@gmail.com>
2023-03-19 13:56:20 +0530

    Changes in banner Sectio , Make banner  More Responsive

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,8 @@ import { Route, Routes } from 'react-router-dom';
 
 import Header from './Component/Header/Header';
 import Home from './Component/Home/Home';
-import Category from './Component/Category/Category';
-import SingleProduct from './Component/SingleProduct/SingleProduct'
+import Category from './Component/Home/Category/Category';
+import SingleProduct from './Component/Home/SingleProduct/SingleProduct'
 import Footer from './Component/Footer/Footer';
 import About from './Component/About/About';
 import Cart from './Component/Cart/Cart';
--- client/src/Component/Home/Banner/Banner.jsx
+++ client/src/Component/Home/Banner/Banner.jsx
@@ -8,11 +8,12 @@ const Banner = () => {
   return (
     <React.Fragment>
       <div className="row">
-        <div className="col content">
-          <h1>Sales</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        <div className="col col-xs-12 col-sm-12 col-md-10 col-lg-6 col-xl-6 col-xxl-6 p-5 content">
+          <h1>SALES</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation </p>
+          <button className="btn btn-outline-light" >SHOP NOW</button>
         </div>
-        <div className="col image">
+        <div className="col col-lg-6 col-xl-6 col-xxl-6 p-2 d-none d-sm-block d-sm-none d-md-block d-md-none d-lg-block image">
           <img className="banner-img" src={BannerImage} alt="banner-img" />
         </div>
       </div>
