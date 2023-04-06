f6f0d073cefd7699de818df5f939293291b821ad
AbhiMane07012000 <abhi.mane07012000@gmail.com>
2023-04-06 20:08:46 +0530

    Project Completed.

--- client/src/Component/About/About.jsx
+++ client/src/Component/About/About.jsx
@@ -32,7 +32,7 @@ const About = () => {
           </div>
           <div className=" col col-lg-6 col-md-12 col-sm-12">
             <h4>Enquiry</h4>
-            <form>
+            <form action="abhijeet.nitin.mane@gmail.com" method="get" enctype="text/plain">
               <label for="fullName" class="pt-3 form-label">
                 Full Name
               </label>
